cae6fc090be4bb28cf0c8c53e7898954acf08f84
liutong <liutong@secdeer.com>
2020-09-23 11:49:58 +0800

    本地仓库初始化提交

---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="Layout-container">
+    </div>
+</template>
+
+<script lang="ts">
+  import {Options, Vue} from 'vue-class-component';
+
+  @Options({})
+  export default class Layout extends Vue {
+  }
+</script>
+
+<style scoped lang="less">
+    .Layout-container {
+
+    }
+</style>
